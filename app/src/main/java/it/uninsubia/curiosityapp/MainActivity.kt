29cc8ea11a076c3f9ccc1578e0718fdfd15895ff
CuriositySabo <CuriosityProjectPDM@gmail.com>
2022-05-16 12:32:20 +0200

    Collegamento firebase db

--- app/src/main/java/it/uninsubia/curiosityapp/MainActivity.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/MainActivity.kt
@@ -2,10 +2,26 @@ package it.uninsubia.curiosityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(auth.currentUser == null) {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+
     }
 }
