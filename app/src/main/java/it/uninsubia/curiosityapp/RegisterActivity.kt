2e1aadd048a76bea98c735274afd59f8d87b1c91
CuriositySabo <CuriosityProjectPDM@gmail.com>
2022-05-16 16:26:34 +0200

    aggiunta logica layout

--- app/src/main/java/it/uninsubia/curiosityapp/RegisterActivity.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/RegisterActivity.kt
@@ -1,11 +1,58 @@
 package it.uninsubia.curiosityapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import it.uninsubia.curiosityapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var layout : ActivityRegisterBinding
+    private lateinit var etnome: EditText
+    private lateinit var etcognome: EditText
+    private lateinit var etemail: EditText
+    private lateinit var etpassword : EditText
+    private lateinit var button: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        val auth = Firebase.auth
+        layout = ActivityRegisterBinding.inflate(layoutInflater)
+
+
+
+        etnome = layout.editTextName
+        etcognome = layout.editTextCognome
+        etemail = layout.editTextEmail
+        etpassword = layout.editTextEmail
+        button = layout.button
+
+
+        button.setOnClickListener() {
+            registerUser()
+        }
+
+
+
+        setContentView(layout.root)
+
+    }
+
+    private fun registerUser() {
+        auth.createUserWithEmailAndPassword(etemail.text.toString(), etpassword.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
     }
 }
