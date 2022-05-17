65767c3ef963f0fd6f944052cf2be4bd954589b1
CuriositySabo <CuriosityProjectPDM@gmail.com>
2022-05-17 16:30:42 +0200

    logica login funzionante

--- app/src/main/java/it/uninsubia/curiosityapp/LoginActivity.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/LoginActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import it.uninsubia.curiosityapp.databinding.ActivityLoginBinding
 import it.uninsubia.curiosityapp.databinding.ActivityRegisterBinding
 
@@ -22,12 +23,17 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
 
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        layout = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
+        auth = Firebase.auth
+
+        layout = ActivityLoginBinding.inflate(layoutInflater)
         etEmail = layout.editTextEmail
         etPassword = layout.editTextPassword
         progressBar = layout.progressbar
@@ -36,17 +42,61 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         button.setOnClickListener(this)
 
         tvRegistrati = layout.registerTv
-        tvRegistrati.setOnClickListener() {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
+        tvRegistrati.setOnClickListener(this)
 
 
         setContentView(layout.root)
     }
 
     private fun loginUser() {
-//        Log.d("TAG", "==========FUNZIONA===========")
+        val email = etEmail.text.toString().trim()
+        val password = etPassword.text.toString().trim()
+
+        if (email.isEmpty()) {
+            etEmail.error = "Inserisci una email"
+            etEmail.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.error = "É necessario inserire una email esistente"
+            etEmail.requestFocus()
+        }
+
+        if (password.isEmpty()) {
+            etPassword.error = "é richiesta una password"
+            etPassword.requestFocus()
+            return
+        }
+
+        if (password.length < 6) {
+            etPassword.error = "é richiesta una password di più di 6 caratteri"
+            etPassword.requestFocus()
+            return
+        }
+
+        progressBar.visibility = View.GONE
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Log in effettuato con successo!",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Errore nel log in",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 
     override fun onClick(v: View?) {
@@ -56,7 +106,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.registerTv -> {
-
+                val intent = Intent(this, RegisterActivity::class.java)
+                startActivity(intent)
             }
         }
     }
--- app/src/main/java/it/uninsubia/curiosityapp/RegisterActivity.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/RegisterActivity.kt
@@ -17,13 +17,15 @@ import com.google.firebase.ktx.Firebase
 import it.uninsubia.curiosityapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var layout : ActivityRegisterBinding
+    private lateinit var layout: ActivityRegisterBinding
+
     private lateinit var etnome: EditText
     private lateinit var etcognome: EditText
     private lateinit var etemail: EditText
-    private lateinit var etpassword : EditText
+    private lateinit var etpassword: EditText
     private lateinit var button: Button
-    private lateinit var progressBar : ProgressBar
+    private lateinit var progressBar: ProgressBar
+
     private lateinit var auth: FirebaseAuth
 
     val DBURL = "https://curiosity-db178-default-rtdb.firebaseio.com"
@@ -31,10 +33,10 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
         auth = Firebase.auth
 
         layout = ActivityRegisterBinding.inflate(layoutInflater)
-
         etnome = layout.editTextName
         etcognome = layout.editTextCognome
         etemail = layout.editTextEmail
@@ -42,63 +44,56 @@ class RegisterActivity : AppCompatActivity() {
         button = layout.registerbutton
         progressBar = layout.progressbar
 
-
         button.setOnClickListener() {
             registerUser()
         }
 
-
-
         setContentView(layout.root)
-
     }
 
-    private fun registerUser( ){
+    private fun registerUser() {
         val nome = etnome.text.toString().trim()
         val cognome = etcognome.text.toString().trim()
         val email = etemail.text.toString().trim()
-        val password =  etpassword.text.toString().trim()
+        val password = etpassword.text.toString().trim()
 
 
-        if(nome.isEmpty()) {
+        if (nome.isEmpty()) {
             etnome.error = "é richiesto il tuo nome"
             etnome.requestFocus()
             return
         }
 
-        if(cognome.isEmpty()) {
+        if (cognome.isEmpty()) {
             etcognome.setError("é richiesto il tuo cognome")
             etcognome.requestFocus()
             return
         }
 
-        if(email.isEmpty()) {
+        if (email.isEmpty()) {
             etemail.error = "é richiesta una email"
             etemail.requestFocus()
             return
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             etemail.error = "É necessario inserire una email esistente"
             etemail.requestFocus()
         }
 
-        if(password.isEmpty()) {
+        if (password.isEmpty()) {
             etpassword.error = "é richiesta una password"
             etpassword.requestFocus()
             return
         }
 
-        if(password.length < 6){
+        if (password.length < 6) {
             etpassword.error = "é richiesta una password di più di 6 caratteri"
             etpassword.requestFocus()
             return
         }
 
-
-
         progressBar.visibility = View.VISIBLE
-
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -107,12 +102,19 @@ class RegisterActivity : AppCompatActivity() {
                     FirebaseDatabase.getInstance(DBURL).getReference("users")
                         .child(FirebaseAuth.getInstance().currentUser!!.uid)
                         .setValue(user).addOnCompleteListener(this) { task ->
-                            if(task.isSuccessful) {
-                                Toast.makeText(this, "Utente registrato correttamente!", Toast.LENGTH_LONG).show()
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    this,
+                                    "Utente registrato correttamente!",
+                                    Toast.LENGTH_LONG
+                                ).show()
                                 progressBar.visibility = View.GONE
-                            }
-                            else {
-                                Toast.makeText(this, "Errore nel registrare l'utente! Riprova!", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    "Errore nel registrare l'utente! Riprova!",
+                                    Toast.LENGTH_LONG
+                                ).show()
                                 progressBar.visibility = View.GONE
                             }
                         }
@@ -120,9 +122,9 @@ class RegisterActivity : AppCompatActivity() {
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
-                }
-                else {
-                    Toast.makeText(this, "Errore nel registrare l'utente!", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Errore nel registrare l'utente!", Toast.LENGTH_LONG)
+                        .show()
                     progressBar.visibility = View.GONE
                 }
             }
