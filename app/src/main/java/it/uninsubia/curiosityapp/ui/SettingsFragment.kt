afd1f973ffbe21a6e6e4a88fadae93aa7d652e28
Lorenzo Bossio <lorenzo.bossio@gmail.com>
2022-05-22 16:46:24 +0200

    added SettingsFragment.kt fragment renamed SubjectSelectionFragment.kt fragment renamed fragment_subject_selection.xml layout added root_preferences.xml as settings layout.

--- app/src/main/java/it/uninsubia/curiosityapp/ui/SettingsFragment.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/ui/SettingsFragment.kt
@@ -1,33 +1,12 @@
 package it.uninsubia.curiosityapp.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import it.uninsubia.curiosityapp.databinding.FragmentSettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import it.uninsubia.curiosityapp.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
