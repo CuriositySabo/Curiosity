ad8fe4dbaa0528edb4c18b5cc1b8b5fe7700939d
Lorenzo Bossio <CuriosityProjectPDM@gnmail.com>
2022-05-25 12:15:08 +0200

    added activity and layout for curiosity+. added home fragment button a listener. added icons e strings

---
+++ app/src/main/java/it/uninsubia/curiosityapp/ui/CuriosityPlus.kt
@@ -0,0 +1,12 @@
+package it.uninsubia.curiosityapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.uninsubia.curiosityapp.R
+
+class CuriosityPlus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_curiosity_plus)
+    }
+}
--- app/src/main/java/it/uninsubia/curiosityapp/ui/HomeFragment.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/ui/HomeFragment.kt
@@ -1,14 +1,17 @@
 package it.uninsubia.curiosityapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import it.uninsubia.curiosityapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var curiosityPlusButton : Button
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,6 +25,12 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        curiosityPlusButton = _binding!!.play
+        curiosityPlusButton.setOnClickListener{
+            val intent = Intent(context,CuriosityPlus::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
         return root
     }
 
