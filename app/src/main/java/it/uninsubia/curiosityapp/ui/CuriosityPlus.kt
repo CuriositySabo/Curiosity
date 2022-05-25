0b935d4daea46ce9ad801db123abd0b3b827972e
Lorenzo Bossio <CuriosityProjectPDM@gmail.com>
2022-05-25 23:06:28 +0200

    added icon for play the game. added strings. added listener for imageView -> added timer that fills up a progressbar.

--- app/src/main/java/it/uninsubia/curiosityapp/ui/CuriosityPlus.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/ui/CuriosityPlus.kt
@@ -2,11 +2,50 @@ package it.uninsubia.curiosityapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import it.uninsubia.curiosityapp.R
 
 class CuriosityPlus : AppCompatActivity() {
+    private lateinit var layoutContainer: LinearLayout
+    private lateinit var buttonStart: ImageView
+    private lateinit var layoutStart: FrameLayout
+    private lateinit var layoutProgressbar: FrameLayout
+    private lateinit var layoutGame: LinearLayout
+    private lateinit var progressBar: ProgressBar
+
+    private lateinit var countdown : CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_curiosity_plus)
+
+        layoutContainer = findViewById(R.id.game_container_layout)
+        buttonStart = findViewById(R.id.imageView_play)
+        layoutStart = findViewById(R.id.start_layout)
+        layoutGame = findViewById(R.id.game_internal_container)
+        layoutProgressbar = findViewById(R.id.progress_bar_start)
+        progressBar = findViewById(R.id.progress_bar_item)
+
+        buttonStart.setOnClickListener {
+            layoutStart.visibility = View.GONE
+            layoutProgressbar.visibility = View.VISIBLE
+            countdown = object: CountDownTimer(1000, 20){
+                override fun onTick(p0: Long) {
+                    if(progressBar.progress < progressBar.max)
+                        progressBar.progress +=2
+                }
+
+                override fun onFinish() {
+                    layoutProgressbar.visibility = View.GONE
+                    layoutGame.visibility = View.VISIBLE
+                    layoutContainer.requestLayout()
+                }
+            }.start()
+        }
     }
 }
