728e672c73f4d11b1f4bdeb97b36d67dba5d8407
CuriositySabo <CuriosityProjectPDM@gmail.com>
2022-05-29 17:17:26 +0200

    changing precedent logic to the one to use a innested Hasmap to write the file

--- app/src/main/java/it/uninsubia/curiosityapp/KnownCuriositiesData.kt
+++ app/src/main/java/it/uninsubia/curiosityapp/KnownCuriositiesData.kt
@@ -1,7 +1,16 @@
 package it.uninsubia.curiosityapp
 
 data class KnownCuriositiesData(var knowncuriosities: HashMap<String, HashMap<Int, Boolean>>) {
-    constructor() : this (hashMapOf<String, HashMap<Int, Boolean>>())
+
+    constructor() : this(hashMapOf<String, HashMap<Int, Boolean>>())
+
+    fun getSize(): Int {
+        var count = 0
+        knowncuriosities.forEach() {
+            count += it.value.size
+        }
+        return count
+    }
 }
 
 //map <code, boolean>
